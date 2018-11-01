3d407c54c53a3ef60d022acc8976d83069edec7a
Numbtus <44679951+Numbtus@users.noreply.github.com>
2018-11-01 22:42:08 +0100

    Update patch.js

--- commands/patch.js
+++ commands/patch.js
@@ -4,9 +4,8 @@ module.exports.run = async (bot , message , args ) => {
          let patchEmbed = new Discord.RichEmbed()
              .setDescription('Affiche les different patch notes des mise a jour precedente')
              .setColor('#1e85ca')
-             .addField('0.3.5',' Ajout de la commande : <help , 
-                       Modification du <info le nombre de serveur et la version du bot sont maintenant disponibles')
-             .addField ('Commande en dev', 'Soyez Patient' );
+             .addField('0.3.7',' Ajout de la commande : <help')
+             .addField ('0.3.7', ' Modification du <info le nombre de serveur et la version du bot sont maintenant disponibles');
              return message.channel.send(patchEmbed);
 
 };
