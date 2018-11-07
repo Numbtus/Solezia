776dfb33406327077a290c731b84c17617db996c
Numbtus <44679951+Numbtus@users.noreply.github.com>
2018-11-07 21:04:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.on('message', async message => {
      if (message.author.bot) return;
      if (message.channel.type === 'dm') return;
 
-     let prefix = config.prefix;
+     let prefix = process.env.PREFIX;
      let messageArray = message.content.split(" ");
      let command = messageArray[0];
      let args = messageArray.slice(1);
@@ -41,4 +41,4 @@ bot.on('message', async message => {
   
 });
 
-bot.login(config.token);
+bot.login(process.env.BOT_TOKEN);
